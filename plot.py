4a3699727c9402040aabe4ff9aacc0b75d5e3720
AravindKrishnanA <65639949+AravindKrishnanA@users.noreply.github.com>
2023-04-30 03:41:23 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,16 +7,10 @@ import opencvt
 
 def plotting(coords):
     serial = serial_reader.Readings()
-    #print(serial)
     if serial:
-        if serial[0] == 1: # or serial[0] == 0: #change this
-            #print("cue ball is in position(3, 3) and second ball is at (5, 5)")
-            cue_ball_center = (3, 3)
-            second_ball_center = (15,15)
-
+        if serial[0] == 1:
             cue_ball_center, second_ball_center = coords[0], coords[1]
             cue_angle = serial[1]
-            #print(cue_angle)
             return physics.lines(cue_angle, cue_ball_center, second_ball_center)
     return ((0),(0))
         
@@ -35,9 +29,6 @@ corners_x = [0, 0, 20, 20, 10, 10]
 corners_y = [0, 20, 0, 20, 0, 20]
 ax.scatter(corners_x, corners_y, s=1000, color='#654321')
 
-# ax.scatter(3, 3, s=500, color='white')
-# ax.scatter(15, 15, s=1500, color='gray')
-# ax.scatter(15, 15, s=500, color='black')
 coords = opencvt.get_coords();
 ax.scatter(coords[0][0], coords[0][1], s=500, color='white')
 ax.scatter(coords[1][0], coords[1][1], s=1500, color='gray')
@@ -49,9 +40,6 @@ line, = ax.plot([], [], lw=2)
 
 # Define a function to update the line data
 def update(num):
-    # Generate 3 random points
-    # x = [random.randint(1, 10) for i in range(3)]
-    # y = [random.randint(1, 10) for i in range(3)]
     plot = plotting(coords)
     x = plot[0]
     y = plot[1]
